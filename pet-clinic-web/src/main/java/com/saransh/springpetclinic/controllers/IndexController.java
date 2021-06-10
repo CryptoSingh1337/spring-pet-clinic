2701d46e99104c8446c35cf1434d4787bb91e163
Saransh Kumar <cryptosingh@gmail.com>
2021-06-10 18:52:12 +0530

    Closes #8: Add Pet Clinic Index Page and Controller.

---
+++ pet-clinic-web/src/main/java/com/saransh/springpetclinic/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.saransh.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by CryptoSingh1337 on 6/10/2021
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/", "index", "index.html"})
+    public String indexPage() {
+        return "index";
+    }
+}

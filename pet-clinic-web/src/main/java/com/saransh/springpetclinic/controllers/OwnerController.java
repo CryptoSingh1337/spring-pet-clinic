79ac18328b42e6bd9bcd7f8b65ac39d331512311
Saransh Kumar <cryptosingh@gmail.com>
2021-06-11 19:16:25 +0530

    Closes #16: List all Owners on Owner index page.

--- pet-clinic-web/src/main/java/com/saransh/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/saransh/springpetclinic/controllers/OwnerController.java
@@ -1,6 +1,8 @@
 package com.saransh.springpetclinic.controllers;
 
+import com.saransh.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,8 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }

ac076cf31cccc303ad7418e241b550fc344b198f
Saransh Kumar <cryptosingh@gmail.com>
2021-06-15 16:01:54 +0530

    Closes #26: Create Pet Type, Pet and Visit Entities.

--- pet-clinic-data/src/main/java/com/saransh/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/saransh/springpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package com.saransh.springpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by CryptoSingh1337 on 6/10/2021
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }

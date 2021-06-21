6638cee0aa1c299ebe0cf101beee0dd6c3837b56
Saransh Kumar <cryptosingh@gmail.com>
2021-06-21 12:38:31 +0530

    Closes #38: Create Visit Entity.

--- pet-clinic-data/src/main/java/com/saransh/springpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/saransh/springpetclinic/model/Visit.java
@@ -1,14 +1,23 @@
 package com.saransh.springpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by CryptoSingh1337 on 6/15/2021
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
